14374d0c28d0d8b6fbc4ff3777d336e5ab7baf7c
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-12-29 19:53:33 -0300

    feat: import routes main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./routes/Home";
+import Countdown from "./routes/Countdown";
+import { CountdownProvider } from "./context/CountdownContext";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/countdown",
+        element: <Countdown />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <CountdownProvider>
+      <RouterProvider router={router} />
+    </CountdownProvider>
+  </React.StrictMode>
+);
